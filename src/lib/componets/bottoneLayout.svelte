22ff13ec8f5d9a68ad9eae60bec7f4e190318abf
Alex3725 <alexprevitali637@gmail.com>
2025-06-24 16:39:59 +0200

    salvataggio prima drizzle

--- src/lib/componets/bottoneLayout.svelte
+++ src/lib/componets/bottoneLayout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-let {testo, percorsoPag} = $props();
+let {testo, percorsoPag, classStyle=" caret-zinc-50 bg-blue-900"} = $props();
 
 
 
 </script>
 
 
-<a href="{percorsoPag}" class="w-96 p-[2%]    caret-zinc-50 bg-blue-900 rounded-[2vw]  ">{testo}</a>
+<a href="{percorsoPag}" class="w-[20%] p-[2%] rounded-[2vw] flex justify-center items-center  {classStyle}">{testo}</a>
