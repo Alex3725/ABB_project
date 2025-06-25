b88f0b14b03e9d0e4f7a6740768ba620e81ee173
Alex3725 <alexprevitali637@gmail.com>
2025-06-25 11:35:12 +0200

    fix

---
+++ src/routes/home/+layout.svelte
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+	import '../../app.css';
+
+	let { children } = $props();
+
+
+	
+</script>
+
+<div class="w-screen h-screen flex flex-col justify-center items-center bg-[url(/img/sfondoLogin.jpg)] bg-cover bg-center">
+	<h1 class=" text-2xl [50%] text-white ">ABB PCTO</h1>
+	<div class=" w-[70%] h-[50%] "><!--  guardare classi su tailwind.css -->
+		{@render children()} <!-- quella parte codice che rappresenta la pagina/pagine-->
+	</div>
+</div>
